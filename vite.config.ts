e53d7aa7b347c904134019c1a9264ffe066eeca5
rishishpradhan16 <rishish.pradhan@gmail.com>
2025-08-14 12:52:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Ajnabi-Chat/", // 👈 apne GitHub repo ka naam yaha likho
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
